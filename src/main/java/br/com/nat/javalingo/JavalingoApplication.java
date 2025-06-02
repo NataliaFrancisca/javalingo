0453014dfadde13ae883a5915ba33c94bdaf8792
nataliafrancisca <nathifrancisca@gmail.com>
2025-06-02 19:59:07 -0300

    feat: iniciando repositorio

---
+++ src/main/java/br/com/nat/javalingo/JavalingoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nat.javalingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalingoApplication.class, args);
+	}
+
+}
