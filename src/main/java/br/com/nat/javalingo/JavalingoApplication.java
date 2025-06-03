dc7d4a5c5c80e910d9f288048459ed55fbc9332c
nataliafrancisca <nathifrancisca@gmail.com>
2025-06-03 00:02:34 -0300

    feat: fiz a injeção do repositorio na classe principal

--- src/main/java/br/com/nat/javalingo/JavalingoApplication.java
+++ src/main/java/br/com/nat/javalingo/JavalingoApplication.java
@@ -1,13 +1,24 @@
 package br.com.nat.javalingo;
 
+import br.com.nat.javalingo.principal.Principal;
+import br.com.nat.javalingo.repository.PalavraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavalingoApplication {
+public class JavalingoApplication implements CommandLineRunner {
+	@Autowired
+	private PalavraRepository palavraRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavalingoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(palavraRepository);
+		principal.exibirMenu();
+	}
 }
