78247377ffea06f1fa64af1c3439289232b4b36f
nataliafrancisca <nathifrancisca@gmail.com>
2025-06-04 23:17:32 -0300

    feat: refatorei algumas queries e criei uma nova para buscar palavras que contenham determinado texto.

--- src/main/java/br/com/nat/javalingo/repository/PalavraRepository.java
+++ src/main/java/br/com/nat/javalingo/repository/PalavraRepository.java
@@ -3,6 +3,7 @@ package br.com.nat.javalingo.repository;
 import br.com.nat.javalingo.model.Palavra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +12,9 @@ public interface PalavraRepository extends JpaRepository<Palavra, Long> {
     @Query("SELECT p FROM Palavra p WHERE p.nivelAprendizado BETWEEN :minimo AND :maximo")
     List<Palavra> buscarPalavrasPorNivelAprendizado(int minimo, int maximo);
 
-    @Query("SELECT p FROM Palavra p WHERE UPPER(p.original) ILIKE UPPER(%:usuarioPesquisa%) OR UPPER(p.traducao) ILIKE UPPER(%:usuarioPesquisa%)")
-    List<Palavra> buscarPalavras(String usuarioPesquisa);
-
-    @Query("SELECT p FROM Palavra p WHERE UPPER(p.original) ILIKE UPPER(%:traducao%) AND UPPER(p.traducao) ILIKE UPPER(%:original%)")
+    @Query("SELECT p FROM Palavra p WHERE UPPER(p.original) ILIKE UPPER(%:traducao%) OR UPPER(p.traducao) ILIKE UPPER(%:original%)")
     Optional<Palavra> buscarPalavraPorAmbosCampos(String original, String traducao);
 
-    Optional<Palavra> findPalavraByTraducaoIgnoreCase(String traducao);
+    @Query("SELECT p FROM Palavra p WHERE p.original ILIKE CONCAT('%', :palavra, '%') OR p.traducao ILIKE CONCAT('%', :palavra, '%')")
+    List<Palavra> buscarPalavrasQueContenhamBusca(@Param("palavra") String palavra);
 }
